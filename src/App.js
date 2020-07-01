8cfff25de712dce07ab3bd1d4743c46f72c2a3a2
ken zhao <ken.zhao42@gmail.com>
2020-07-01 12:09:36 -0400

    aligned text on roll button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Dice from './components/dice'
+import Dice from './components/dice_app'
 
 function App() {
   return (
