9f747853b78ad607483a0b27a25b07ae34b8dd40
ken zhao <ken.zhao42@gmail.com>
2020-06-30 23:23:09 -0400

    Updates

---
+++ precache-manifest.91b802b793bd8c6a16e3bd122b62327b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0322dbe8e79a724a2cc9cb6395d9f3e0",
+    "url": "/dice-simulator/index.html"
+  },
+  {
+    "revision": "973f0aefe33102f672e5",
+    "url": "/dice-simulator/static/css/main.85d250a2.chunk.css"
+  },
+  {
+    "revision": "551fac14807f67eb6f67",
+    "url": "/dice-simulator/static/js/2.2601f86c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice-simulator/static/js/2.2601f86c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973f0aefe33102f672e5",
+    "url": "/dice-simulator/static/js/main.7692ec22.chunk.js"
+  },
+  {
+    "revision": "0f80ea3b4b2f230d38dc",
+    "url": "/dice-simulator/static/js/runtime-main.4c768697.js"
+  },
+  {
+    "revision": "e1aa13c9e7bb08fad320e2388f277131",
+    "url": "/dice-simulator/static/media/dice-six-faces-five.e1aa13c9.png"
+  },
+  {
+    "revision": "8c0d932b0896b704e7f882132a691df7",
+    "url": "/dice-simulator/static/media/dice-six-faces-four.8c0d932b.png"
+  },
+  {
+    "revision": "3baf8c446cc8570b8885c1a72ee28051",
+    "url": "/dice-simulator/static/media/dice-six-faces-one.3baf8c44.png"
+  },
+  {
+    "revision": "113da4a6d4944fba5e83d9e9e413b2b4",
+    "url": "/dice-simulator/static/media/dice-six-faces-six.113da4a6.png"
+  },
+  {
+    "revision": "796b7fbb18968e7c54f13d9b7e9e76a1",
+    "url": "/dice-simulator/static/media/dice-six-faces-three.796b7fbb.png"
+  },
+  {
+    "revision": "fbbd43052ad538d6c857376c0fc2f8cb",
+    "url": "/dice-simulator/static/media/dice-six-faces-two.fbbd4305.png"
+  }
+]);
